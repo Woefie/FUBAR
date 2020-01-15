3ce983b51e63cb73ab680300dbd9d731b321cf98
Woefie <larsh.hanssen@gmail.com>
2020-01-15 14:29:53 +0100

    Added ADC code

---
+++ main/Peripherals/adc.h
@@ -0,0 +1,7 @@
+#ifndef ADC_H
+#define ADC_H
+
+#define DEFAULT_VREF 1100 //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES 64  //Multisampling
+
+#endif
--- main/Peripherals/analog.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
-
